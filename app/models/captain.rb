4aa805f1f4411a8f2dc03f265f479045d5baaacc
Nicky Liu <nickyliu91@gmail.com>
2018-09-11 06:18:25 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    self.joins(:boats => :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    self.joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seafarers
+    self.joins(boats: :classifications).where("classifications: {name: 'Sailboat'} AND classifications: {name: 'Motorboat'}").uniq
+  end
+
+  def self.non_sailors
+    self.joins(boats: :classifications).where.not(classifications: {name: "Sailboat"}).uniq
+  end
 end
